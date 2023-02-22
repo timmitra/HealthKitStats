a8fd811b6e6f3a3193138fe1e72d95be0f9631bd
Tim Mitra <tim@it-guy.com>
2023-02-21 23:45:51 -0500

    Initial Commit

---
+++ HealthKitStats/HealthKitStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitStatsApp.swift
+//  HealthKitStats
+//
+//  Created by Tim Mitra on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
